ce26d8df8e5d0e6f429760b95e9a58fb1d595ab8
Android BTI <android.bti@A309-07L.imd.local>
2018-08-30 17:25:48 -0300

    update

--- Fragment_communication/app/src/main/java/com/example/frag_communication/MainActivity.java
+++ Fragment_communication/app/src/main/java/com/example/frag_communication/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.frag_communication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- Fragment_communication/app/src/main/java/com/example/frag_communication/textFragment.java
+++ Fragment_communication/app/src/main/java/com/example/frag_communication/textFragment.java
@@ -5,10 +5,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class textFragment extends Fragment{
+
+    private EditText edittext;
+    private Button btn;
+
     @Override
     public View onCreateView(LayoutInflater inflater,  ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.frag_text,container,false);
+        View view = inflater.inflate(R.layout.frag_text,container,false);
+        edittext = view.findViewById(R.id.editText);
+        btn = view.findViewById(R.id.btnButton);
+
+        return view;
     }
 }
