0beba13be73b108f48d975a0404226a04963f6c6
Android BTI <android.bti@A309-07L.imd.local>
2018-09-11 18:02:26 -0300

    adjustments

--- ExemploListFragment/app/src/main/java/com/example/exemplolistfragment/MainActivity.java
+++ ExemploListFragment/app/src/main/java/com/example/exemplolistfragment/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.exemplolistfragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity implements AlimentosFragment.OnItemClick {
 
     private TextFragment text_fragment;
+    private static final NumberFormat np = NumberFormat.getCurrencyInstance(new Locale("pt","BR"));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +22,7 @@ public class MainActivity extends AppCompatActivity implements AlimentosFragment
     @Override
     public void onClick(Alimento alimento) {
 
-        text_fragment.setText(String.format("O preco do %s eh %s", alimento.getNome(), alimento.getPreco()));
+        text_fragment.setText(String.format("O preco do %s eh %s", alimento.getNome(), np.format(alimento.getPreco())));
 
     }
 }
