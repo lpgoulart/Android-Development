ce1f58a3efe190d34511744d871e16ea82c86202
Android BTI <android.bti@A309-08L.imd.local>
2018-10-09 18:34:08 -0300

    sidebar nav

--- NavigationDrawer/app/src/main/java/com/example/androidbti/navigationdrawer/MainActivity.java
+++ NavigationDrawer/app/src/main/java/com/example/androidbti/navigationdrawer/MainActivity.java
@@ -2,20 +2,28 @@ package com.example.androidbti.navigationdrawer;
 
 import android.app.SearchManager;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.SlidingDrawer;
 
 public class MainActivity extends AppCompatActivity implements PlanetAdapter.OnItemClickListener{
 
     private DrawerLayout drawerLayout;
     private ListView drawerList;
     private Planet planetselecter;
+
+    private ActionBarDrawerToggle drawerToggle;
 
 
     @Override
@@ -28,13 +36,53 @@ public class MainActivity extends AppCompatActivity implements PlanetAdapter.OnI
 
         drawerList.setAdapter(new PlanetAdapter(this, this));
 
-        selectItem(null);
+        if ( getSupportActionBar() != null ) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+
+        }
+
+        drawerToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.drawer_open,R.string.drawer_close){
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                setTitle(R.string.title_choose);
+                invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                if (planetselecter != null) {
+                    setTitle(planetselecter.getName());
+                } else {
+                    setTitle(R.string.app_name);
+                }
+                invalidateOptionsMenu();
+            }
+        };
+
+        if( savedInstanceState == null ) {
+            selectItem(null);
+        }else {
+            planetselecter = (Planet) savedInstanceState.getSerializable("planet");
+            selectItem(planetselecter);
+        }
+
+        drawerLayout.addDrawerListener(drawerToggle);
 
     }
 
     @Override
     public void onClick(Planet planet) {
         selectItem(planet);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        boolean drawerOpen = drawerLayout.isDrawerOpen(drawerList);
+        MenuItem searchMenu = menu.findItem(R.id.action_search);
+        searchMenu.setVisible(!drawerOpen && planetselecter!=null);
+
+        return super.onPrepareOptionsMenu(menu);
     }
 
     @Override
@@ -45,6 +93,10 @@ public class MainActivity extends AppCompatActivity implements PlanetAdapter.OnI
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
         if ( item.getItemId() == R.id.action_search ) {
             Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
             intent.putExtra(SearchManager.QUERY,"Planet"+planetselecter.getName());
@@ -62,5 +114,23 @@ public class MainActivity extends AppCompatActivity implements PlanetAdapter.OnI
             transaction.commit();
             setTitle(planet.getName());
         }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        outState.putSerializable("planet",planetselecter);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
     }
 }
